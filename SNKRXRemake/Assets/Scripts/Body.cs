8d2db5f2cfd8c40c3fac0598c1c2fa67a8d51b12
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2021-10-10 12:23:22 +0200

    Project start

---
+++ SNKRXRemake/Assets/Scripts/Body.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Body
+{
+    public class Body : MonoBehaviour
+    {
+        [SerializeField] private int moveSpeed;
+        [SerializeField] private int health;
+        [SerializeField] private int level;
+        [SerializeField] private double angle;
+        [SerializeField] private double xPos;
+        [SerializeField] private double yPos;
+
+    }
+}
---
+++ SNKRXRemake/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Body
+{
+    public class Movement : Body
+    {
+        void Update()
+        {
+            /*
+            xPos = transfor
+            yPos =
+            xMove = Math.Cos(angle) * this.moveSpeed;
+            xMove = Math.Sin(angle) * this.moveSpeed;
+            this.xPos += xMove;
+            this.yPos += yMove;
+            */
+        }
+    }
+
+}
