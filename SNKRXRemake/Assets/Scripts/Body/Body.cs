8400f00f4d7774c61d0e57ca93216510b8242a48
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2021-10-11 13:20:15 +0200

    Some filemanagement and other stuff

---
+++ SNKRXRemake/Assets/Scripts/Body/Body.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Body
+{
+    public class Body : MonoBehaviour
+    {
+        [SerializeField] private int moveSpeed;
+        [SerializeField] private int health;
+        [SerializeField] private int level;
+        private Vector3 moveDirection;
+        private Rigidbody2D m_Rigidbody2D;
+        private bool newSpawn = true;
+    }
+}
---
+++ SNKRXRemake/Assets/Scripts/Body/Movement/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Body
+{
+    public class Movement : Body
+    {
+        void Update()
+        {
+           
+
+        }
+        public void Move() 
+        {
+            Vector3 targetVel = new Vector2(moveSpeed)
+        }
+    }
+
+}
